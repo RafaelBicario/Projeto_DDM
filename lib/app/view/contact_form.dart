b17c93d95dab17af7d80a6106a6bd1fb74756738
RafaelBicario <rafabicario@outlook.com>
2021-07-21 21:12:35 -0300

    "finalizando CRUD"

--- lib/app/view/contact_form.dart
+++ lib/app/view/contact_form.dart
@@ -4,8 +4,12 @@ import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'contact_form_back.dart';
 
 class ContactForm extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
+
   Widget fieldRaca(ContactFormBack back) {
     return TextFormField(
+        validator: back.validateRaca,
+        onSaved: (newValue) => back.contact.raca = newValue,
         initialValue: back.contact.nome,
         decoration: InputDecoration(
             labelText: 'Raça: ', hintText: 'Informe a Raça do Animal'));
@@ -21,6 +25,8 @@ class ContactForm extends StatelessWidget {
   Widget fieldPhone(ContactFormBack back) {
     var mask = MaskTextInputFormatter(mask: '(##) # ####-####');
     return TextFormField(
+        validator: back.validatePhone,
+        onSaved: (newValue) => back.contact.phone = newValue,
         initialValue: back.contact.phone,
         inputFormatters: [mask],
         keyboardType: TextInputType.number,
@@ -41,11 +47,23 @@ class ContactForm extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cadastro de Atendimentos'),
-        actions: [IconButton(icon: Icon(Icons.save), onPressed: null)],
+        actions: [
+          IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                _form.currentState!.validate();
+                _form.currentState!.save();
+                if (_back.isValid) {
+                  _back.save();
+                  Navigator.of(context).pop();
+                }
+              })
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.all(10),
         child: Form(
+          key: _form,
           child: Column(children: [
             fieldRaca(_back),
             fieldDescricao(_back),
--- lib/app/view/contact_form_back.dart
+++ lib/app/view/contact_form_back.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_application_1/app/domain/services/contact_service.dart';
+import 'package:get_it/get_it.dart';
 import 'package:mobx/mobx.dart';
 
 part 'contact_form_back.g.dart';
@@ -6,9 +8,42 @@ class ContactFormBack = _ContactFormBack with _$ContactFormBack;
 
 abstract class _ContactFormBack with Store {
   Contact contact;
+  var _service = GetIt.I.get<ContactService>();
+  bool _racaIsValid;
+  // bool _descricaoIsValid;
+  bool _phoneIsValid;
+  // bool _valorIsValid;
+
+  bool get isValid => _racaIsValid && _descricaoIsValid && _phoneIsValid;
 
   _ContactFormBack(BuildContext context) {
     var parameter = ModalRoute.of(context).settings.arguments;
     contact = (parameter == null) ? Contact() : parameter;
+  }
+
+  save() async {
+    await _service.save(contact);
+  }
+
+  String validateRaca(String raca) {
+    try {
+      _service.validadeRaca(raca);
+      _racaIsValid = true;
+      return null;
+    } catch (e) {
+      _racaIsValid = false;
+      return e.toString();
+    }
+  }
+
+  String validatePhone(String phone) {
+    try {
+      _service.validadePhone(phone);
+      _phoneIsValid = true;
+      return null;
+    } catch (e) {
+      _phoneIsValid = false;
+      return e.toString();
+    }
   }
 }
