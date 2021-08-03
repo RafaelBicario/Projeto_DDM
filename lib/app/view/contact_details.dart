336da05d419567b706bf1f0c610511bdcd512698
RafaelBicario <rafabicario@outlook.com>
2021-08-03 18:58:03 -0300

    chamada APP externo

--- lib/app/view/contact_details.dart
+++ lib/app/view/contact_details.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app/domain/entities/contact.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'contact_details_back.dart';
 
 class ContactDetails extends StatelessWidget {
+
+  showModalError(){
+    var alert = AlertDialog(
+      title:Text('Alerta'),
+      content:Text('Não foi possível encontrar um APP compatível.'),
+    );
+    showDialog(
+      context:context,
+      builder:(BuildContext context){
+        return alert;
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var _back = ContactDetailsBack(context);
@@ -16,18 +31,40 @@ class ContactDetails extends StatelessWidget {
 
         return Scaffold(
           body: ListView(
+            // padding:EdgeInserts.all(60),
             children: [
               Center(
                 child: Text('${contact.raca}', style: TextStyle(fontSize: 30)),
               ),
               Card(
                 child: ListTile(
-                  title: Text('${contact.telefone}'),
+                  title: Text('Telefone'),
+                  subtitle: Text('${contact.telefone}'),
                 ),
               ),
               Card(
                 child: ListTile(
-                  title: Text('${contact.valor}'),
+                  title: Text('Valor'),
+                  subtitle: Text('${contact.valor}'),
+                  trailing: Container(
+                    width: width/4,
+                     child: Row{ 
+                       children:[
+                         IconButton(
+                           color:Colors.blue,
+                           icon: Icon(Icons.message),
+                           onPressed: (){
+                            _back.launchPhone(showModalError());
+                           }
+                          ),
+                          IconButton(
+                           color:Colors.blue,
+                           icon: Icon(Icons.phone),
+                           onPressed: (){
+                             
+                           })
+                    ]
+                  })
                 ),
               ),
             ],
--- lib/app/view/contact_details_back.dart
+++ lib/app/view/contact_details_back.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app/domain/entities/contact.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactDetailsBack {
   BuildContext context;
@@ -11,5 +12,13 @@ class ContactDetailsBack {
 
   goToBack() {
     Navigator.of(context).pop();
+  }
+
+  _launchApp(String url, Function(BuildContext context) showModalError) async {
+    await canLaunch(url) ? await launch(url) : showModalError(context);
+  }
+
+  launchPhone(Function(BuildContext context) showModalError) {
+    _launchApp('tel:${contact.telefone}', showModalError);
   }
 }
